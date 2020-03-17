2605fa09f5242a3e2840e3c01ca74e762355649a
zelihasezeer@gmail.com <zelihasezer@gmail.com>
2020-03-17 13:55:27 -0500

    to open website

---
+++ src/test/java/com/automation/test/day05/RadioButton.java
@@ -0,0 +1,41 @@
+package com.automation.test.day05;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class RadioButton {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().version("79.0").setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+
+        driver.manage().window().maximize();
+        BrowserUtils.wait(2);
+
+        List<WebElement> radioButtons = driver.findElements(By.tagName("input"));
+
+        for (WebElement radioButton : radioButtons){
+            String id=radioButton.getAttribute("id");
+            if (radioButton.isEnabled()) {
+                radioButton.click();
+                System.out.println("clicked on: " + id);
+                BrowserUtils.wait(1);
+
+            }else{
+                System.out.println("Button is disabled, not clicked : "+id);
+
+
+        }
+    }
+        driver.quit();
+}
+}
