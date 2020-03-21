e4b1fdc6f0c5cb929cd91da422b337dfec67e854
zelihasezeer@gmail.com <zelihasezer@gmail.com>
2020-03-20 23:58:15 -0500

    Verify Page Number

---
+++ src/test/java/com/automation/test/Vytrack/Fleet/VerifyPageNumber.java
@@ -0,0 +1,51 @@
+package com.automation.test.Vytrack.Fleet;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class VerifyPageNumber {
+
+
+    private WebDriver driver;
+    private String URL = "https://qa2.vytrack.com/user/login";
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
+    private By fleetBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Fleet')]");
+    private By subTitleBy = By.className("oro-subtitle");
+    private String username = "storemanager85";
+    private String password = "UserUser123";
+
+
+    @Test
+    public void verifyPageNumber() {
+
+
+    }
+
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
